f25e3630153ed9e93aeb4e0caf4097f00f24a941
zelya2021 <zelenckina1999@gmail.com>
2020-09-30 21:22:25 +0300

    added project

---
+++ TCPChat/TCPChat/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TCPChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lbChatClients = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(374, 568);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(100, 28);
+            this.btnSend.TabIndex = 19;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lbChatClients
+            // 
+            this.lbChatClients.FormattingEnabled = true;
+            this.lbChatClients.ItemHeight = 16;
+            this.lbChatClients.Location = new System.Drawing.Point(23, 13);
+            this.lbChatClients.Margin = new System.Windows.Forms.Padding(4);
+            this.lbChatClients.Name = "lbChatClients";
+            this.lbChatClients.Size = new System.Drawing.Size(460, 516);
+            this.lbChatClients.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(166, 546);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 19);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Enter text";
+            // 
+            // tbText
+            // 
+            this.tbText.Location = new System.Drawing.Point(22, 568);
+            this.tbText.Margin = new System.Windows.Forms.Padding(4);
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(327, 22);
+            this.tbText.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 618);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lbChatClients);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lbChatClients;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbText;
+    }
+}
+
---
+++ TCPChat/TCPChat/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPChat
+{
+    public partial class Form1 : Form
+    {
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        const string IP = "127.0.0.1";
+        const int PORT = 55_555;
+        //User user = null;
+        //конечная точка сервера
+        IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IP), PORT);
+        public Form1()
+        {
+            InitializeComponent();
+            clientSocket.Connect(ep);
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            byte[] data1 = Encoding.UTF8.GetBytes(tbText.Text);
+            //отправка сообщения на сервер
+            clientSocket.Send(data1);
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            byte[] data = new byte[64];
+                            StringBuilder builder = new StringBuilder();
+                            int bytes = 0;
+                            do
+                            {
+                                bytes = clientSocket.Receive(data);
+                                builder.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                            }
+                            while (clientSocket.Available > 0);
+                            lbChatClients.Items.Add(builder.ToString());
+                        }
+                        catch
+                        {
+                            lbChatClients.Items.Add("Error");
+                        }
+                    }
+                }
+                catch
+                {
+                    lbChatClients.Items.Add("Exit");
+                }
+            });
+
+            tbText.Text = "";
+        }
+    }
+}
